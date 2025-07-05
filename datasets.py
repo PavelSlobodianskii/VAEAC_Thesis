6ea079ed9d260a12d6e8e86d5316abb8f66bf875
PavelSlobodianskii <pavel.slobodianskii@gmail.com>
2025-07-05 11:47:41 +0000

    SymmetricWeighted_KL (without switching networks)

--- datasets.py
+++ datasets.py
@@ -167,35 +167,34 @@ def load_dataset(name):
         ToTensor(),
         Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
     ])
-    celeba_root_dir = '/dbstore/datasets/celebA'  # change it for your system!
+    celeba_root_dir = '/content/vaeac/data/vaeac_project'  # change it for your system!
     celeba_img_dir = join(celeba_root_dir, 'img_align_celeba')
     celeba_partition = join(celeba_root_dir, 'list_eval_partition.txt')
 
     if name == 'celeba_train':
-        return CelebA(
+        # Only 15,000 images in training set
+        return LengthBounder(CelebA(
             celeba_img_dir,
             celeba_partition,
             'train',
-            celeba_transforms
-        )
+            celeba_transforms), 15000)
     elif name == 'celeba_val':
-        # in order to speed up training we restrict validation set
-        # to have only 1024 images
+        # 750 images for validation
         return LengthBounder(CelebA(
             celeba_img_dir,
             celeba_partition,
             'valid',
-            celeba_transforms), 1024)
+            celeba_transforms), 750)
     elif name == 'celeba_test':
-        # in order to demonstrate the inpainting results we don't need
-        # the whole test set, so we use 256 test images only
+        # 250 images for test
         return LengthBounder(CelebA(
             celeba_img_dir,
             celeba_partition,
             'test',
-            celeba_transforms), 256)
+            celeba_transforms), 250)
     elif name == 'celeba_inpainting_masks':
         return GeneratorDataset(ImageMaskGenerator(),
                                 load_dataset('celeba_test'))
     else:
         raise ValueError('Unknown dataset %s' % str(name))
+
